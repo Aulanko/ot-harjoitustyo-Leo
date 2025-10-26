dff3601cd24fe64c6498e16c1b090bd817650de6
pronom8 <mangros19@gmail.com>
2025-10-26 11:24:14 +0200

    viikko 2 tehtävä 6 tehty

--- laskarit/viikko2/unicafe/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,21 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_salo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti),"Kortilla on rahaa 10.00 euroa",)
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(30)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.30 euroa")
+
+    def test_rahan_ottaminen_toimii(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+        self.maksukortti.ota_rahaa(9999)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+        onnistuu = self.maksukortti.ota_rahaa(20)
+        self.assertEqual(True, onnistuu)
+
+        
+
