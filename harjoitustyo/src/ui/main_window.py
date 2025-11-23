fb60707305e7a8f0bca3a1cda7d49bca0da1b93b
pronom8 <mangros19@gmail.com>
2025-11-23 15:39:20 +0200

    continuing to build the to be renewed UI

--- harjoitustyo/src/ui/main_window.py
+++ harjoitustyo/src/ui/main_window.py
@@ -1,13 +1,54 @@
 
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout,QHBoxLayout, QLabel, QGridLayout, QTextEdit,
                              QPushButton, QLineEdit)
+from PyQt6.QtCore import QSize, Qt
 import sys
-
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 
 
 class DataTickerWidget(QLabel):
+
+    def __init__(self, symbol:str, price: float, change:float):
+        super().__init__()
+        self.symbol = symbol
+        self.price = price
+        self.change = change
+        self.color = ""
+        self.sign = ""
+        self.set_up_UI()
+
+    def set_up_UI(self):
+        if self.change >=0:
+            self.color ="green"
+            self.sign="+"
+        else:
+            self.color="red"
+            self.sign ="-"
+
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+     
+        self.symbol_label = QLabel(f"{self.symbol}")
+        self.price_label = QLabel(f"${self.price}")
+        self.change_label = QLabel(f"{self.sign}{abs(self.change)}%")
+        self.change_label.setStyleSheet(f"color: {self.color}")
+
+        layout.addWidget(self.symbol_label)
+        layout.addWidget(self.price_label)
+        layout.addWidget(self.change_label)
+
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+
+
+
+
     pass
+
+
 
 
 class MainWindow(QMainWindow):
@@ -74,7 +115,18 @@ class MainWindow(QMainWindow):
 
         self.data_ticker_widgets = {}
         for i, symbol in enumerate(self.CurrentSymbols):
-            print("koira")
+            tick = DataTickerWidget(symbol, 0,0)
+            self.data_ticker_widgets[symbol]= tick
+            layout.addWidget(tick,0,i)
+
+        self.PriceChart = FigureCanvas(Figure(figsize=(10,4)))
+        layout.addWidget(self.PriceChart,1,0,1,len(self.CurrentSymbols))
+
+        return panel
+
+
+
+
 
 
 
