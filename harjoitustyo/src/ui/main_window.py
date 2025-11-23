d42be448f33d3d49f19eff2bfbdcd96ebda491b2
pronom8 <mangros19@gmail.com>
2025-11-23 14:26:50 +0200

    continuing to build the UI

--- harjoitustyo/src/ui/main_window.py
+++ harjoitustyo/src/ui/main_window.py
@@ -1,16 +1,68 @@
 
-from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, QGridLayout, QTextEdit,
-                             QPushButton)
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout,QHBoxLayout, QLabel, QGridLayout, QTextEdit,
+                             QPushButton, QLineEdit)
 import sys
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
         self.setUpUI()
 
+        
+
     def setUpUI(self):
+        self.setWindowTitle("Stock data application")
+
+        center_widget = QWidget()
+        self.setCentralWidget(center_widget)
+        main_layout = QHBoxLayout()
+        center_widget.setLayout(main_layout)
+
+        left_panel = self.create_left_panel()
+        main_layout.addWidget(left_panel)
+
+
+
         pass
+
+    def create_left_panel(self):
+        panel = QWidget()
+        layout = QVBoxLayout()
+        panel.setLayout(layout)
+
+        title = QLabel("Analyze Stocks")
+        layout.addWidget(title)
+
+        layout.addWidget(QLabel("Symbols:"))
+        self.symbols = QTextEdit()
+
+        layout.addWidget(self.symbols)
+
+        symbol_layout = QHBoxLayout()
+        self.new_symbol = QLineEdit()
+        self.new_symbol.setPlaceholderText("Add a new symbol (for example; NVDA)")
+        symbol_layout.addWidget(self.new_symbol)
+
+        add_button = QPushButton("Add")
+        #add_button.clicked.connect()
+        symbol_layout.addWidget(add_button)
+        layout.addLayout(symbol_layout)
+
+        analyze_button = QPushButton("Analyze")
+        #analyze_button.clicked.connect()
+
+        layout.addWidget(analyze_button)
+
+        return panel
+    
+
+
+
+
+
+
 
 
 
@@ -23,5 +75,5 @@ def main_window():
     app.exec()
 
 
-if __name__=="__main_window__":
+if __name__=="__main__":
     main_window()
