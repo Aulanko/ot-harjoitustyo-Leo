ca82d075a76312bef1b1e4c33fadd1f89ed7f3f2
pronom8 <mangros19@gmail.com>
2025-12-05 16:17:23 +0200

    increased the test coverage

--- harjoitustyo/tests/test_stock_repo.py
+++ harjoitustyo/tests/test_stock_repo.py
@@ -1,8 +1,6 @@
 
 import unittest
-import sqlite3
-import os
-import tempfile
+from unittest.mock import MagicMock
 from src.repositories.stock_repo import StockRepository
 
 class Test_Stock_Repository(unittest.TestCase):
@@ -15,7 +13,31 @@ class Test_Stock_Repository(unittest.TestCase):
         repo = StockRepository(db_path=":memory:")
         self.assertIsInstance(repo, StockRepository)
 
-    
+    def test_cache_key(self):
+        vast = self.repo.cache_key("AAPL", "current")
+        self.assertIsInstance(vast, str)
+        self.assertEqual("stock:AAPL:current", vast)
+
+    def test_get_from_cache(self):
+        self.repo.cache_enabled = False
+        self.assertIsNone(self.repo._get_from_cache("testaa_key"))
+        self.repo.redis_client = None
+        self.assertIsNone(self.repo._get_from_cache("testaa_key"))
+
+        self.repo.cache_enabled = True
+        self.repo.redis_client = MagicMock()
+        self.repo.redis_client.get.return_value = "testaa_key"
+        vast = self.repo._get_from_cache("testaa_key")
+
+        self.assertEqual(vast, "testaa_key")
+
+
+
+
+
+
+
+
         
 
         
