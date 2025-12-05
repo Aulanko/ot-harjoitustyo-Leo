26bdad8f9559ad2c6b43079d090c359777607cbb
pronom8 <mangros19@gmail.com>
2025-12-05 17:35:07 +0200

    increasing the test coverage

--- harjoitustyo/tests/test_stock_repo.py
+++ harjoitustyo/tests/test_stock_repo.py
@@ -1,35 +1,53 @@
 
 import unittest
-from unittest.mock import MagicMock
-from src.repositories.stock_repo import StockRepository
+from unittest.mock import Mock, patch
+from src.repositories.stock_repo import StockRepository, InvalidSymbol, StockData, DataFactory
+import pandas as pd
+import yfinance as yf
+
 
 class Test_Stock_Repository(unittest.TestCase):
     def setUp(self):
         self.repo = StockRepository()
-        self.symbols = ["AAPL", "GOOGL", "MSFT"]
+        self.symbols = ["AAPL","GOOGL","MSFT"]
 
-    
-    def test_init_database(self):
-        repo = StockRepository(db_path=":memory:")
-        self.assertIsInstance(repo, StockRepository)
 
-    def test_cache_key(self):
-        vast = self.repo.cache_key("AAPL", "current")
-        self.assertIsInstance(vast, str)
-        self.assertEqual("stock:AAPL:current", vast)
 
     def test_get_from_cache(self):
-        self.repo.cache_enabled = False
-        self.assertIsNone(self.repo._get_from_cache("testaa_key"))
         self.repo.redis_client = None
-        self.assertIsNone(self.repo._get_from_cache("testaa_key"))
+        self.assertIsNone(self.repo._get_from_cache("opa"))
+        self.repo.redis_client = Mock()
+        self.repo.redis_client.get.return_value = "opa"
 
+    
         self.repo.cache_enabled = True
-        self.repo.redis_client = MagicMock()
-        self.repo.redis_client.get.return_value = "testaa_key"
-        vast = self.repo._get_from_cache("testaa_key")
+        self.assertEqual("opa", self.repo._get_from_cache("opa"))
+    
+    def test_init_database(self):
+        rep = StockRepository(db_path=":memory:")
+        self.assertIsInstance(rep, StockRepository)
 
-        self.assertEqual(vast, "testaa_key")
+    
+
+    def test_cache_key(self):
+  
+        k = self.repo.cache_key("AAPL", "current")
+        
+        self.assertIsInstance(k, str)
+        self.assertEqual(k, "stock:AAPL:current")
+
+
+   
+
+
+
+        
+
+        
+
+        
+
+
 
 
 
